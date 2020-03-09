0e34d3e6938c257d1bcd13583c54665b4b0a42ed
jcg678 <jcg678@hotmail.es>
2020-03-09 19:59:03 +0100

    validando cmapos de de registro

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 'use strict'
+var validator =require('validator');
 
 var controller = {
     probando: function (req, res) {
@@ -13,9 +14,26 @@ var controller = {
     },
 
     save: function (req, res) {
+
+        var params = req.body;
+
+        var validate_name = !validator.isEmpty(params.name);
+        var validate_surname = !validator.isEmpty(params.surname);
+        var validate_email = !validator.isEmpty(params.email) && validator.isEmail(params.email);
+        var validate_password = !validator.isEmpty(params.password);
+
+        if(validate_password && validate_name && validate_email && validate_surname){
+
+        }else{
+            return res.status(200).send({
+                message : "Los campos no son validos",
+            });
+        }
+
+
         return res.status(200).send({
-            message : "Registro de usuarios"
-        })
+            message : "Registro de usuarios",
+        });
     }
 };
 
