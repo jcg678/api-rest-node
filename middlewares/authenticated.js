8e633f52e7d77ce2ceeee3ad58a09a645923aa5f
jcg678 <jcg678@hotmail.es>
2020-03-16 19:28:32 +0100

    creando middleware

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -1,6 +1,36 @@
 'use strict'
+var jwt = require('jwt-simple');
+var moment = require('moment');
+
+
+var secret = 'clave-secreta-para-generar-token';
 
 exports.authenticated = function (req, res, next) {
-        console.log('estas pasando por el middleware');
+    if(!req.headers.authorization){
+        return res.status(403).send({
+           message: 'La petición no tine la cabecera de authorization'
+        });
+
+    };
+
+    var token = req.headers.authorization.replace(/['"]+/g, '');
+    
+    try{
+        var payload = jwt.decode(token, secret);
+
+        if(payload.exp <= moment().unix()){
+            return res.status(404).send({
+                message: 'El token a expirado'
+            });
+        }
+    }catch (e) {
+        return res.status(404).send({
+            message: 'El token no es valido'
+        });
+    }
+
+    req.user = payload;
+
+    console.log('estas pasando por el middleware 2');
     next();
-}
+};
