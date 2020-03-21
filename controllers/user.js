a559aeebaa05ef473abf3588f7a787b1de26c20c
jcg678 <jcg678@hotmail.es>
2020-03-21 13:42:54 +0100

    Guardando archivo

--- controllers/user.js
+++ controllers/user.js
@@ -202,10 +202,30 @@ var controller = {
     },
     uploadAvatar: function (req,res) {
 
+        var file_name = 'Avatar no subido...';
+
+        if(!req.files){
+            return res.status(404).send({
+                status: 'error',
+                message: file_name
+            });
+        }
+
+        var file_path = req.files.file0.path;
+
+        ///Cuidado en linux o Mac se utiliza / en lugar de \\
+        var file_split = file_path.split('\\');
+
+        var file_name = file_split[2];
+
+        var ext_split = file_name.split('\.');
+        var file_ext= ext_split[1];
+
 
         return res.status(200).send({
             status: 'success',
-            message: 'Uload avatar'
+            message: 'Uload avatar',
+            file: file_ext
         });
     },
 };
--- routes/user.js
+++ routes/user.js
@@ -6,12 +6,16 @@ var UserController = require('../controllers/user');
 var router = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
+var multipart = require('connect-multiparty');
+
+var md_upload = multipart({uploadDir: './uploads/users'});
+
 router.get('/probando', UserController.probando);
 router.get('/testeando', UserController.testeaando);
 
 router.post('/register',UserController.save);
 router.post('/login', UserController.login);
 router.put('/update', md_auth.authenticated, UserController.update);
-router.post('/upload-avatar',  UserController.uploadAvatar);
+router.post('/upload-avatar', [md_upload,md_auth.authenticated], UserController.uploadAvatar);
 
 module.exports = router;
